daf203e96a73cb6b3a145fa89a7c8629e2fddb16
Nicklas Frederiksen <nfrederiksen@hotmail.com>
2021-10-25 14:15:02 +0200

    Add Ink

---
+++ Assets/Story/story.ink
@@ -0,0 +1,24 @@
+Den første historie
+
+-> start
+
+=== start ===
+Dette er en testhistorie. Hvad siger du til det?
++ [Okay]
+->next
++ [Nej tak]
+->no
+
+=== next ===
+Fedt. Lad os tage et skrift mere.
+* [God ide]
+->cool
+
+=== cool ===
+Fedt! Vi slutter her
+->END
+
+=== no ===
+Okay. Jeg smutter
+#fight
+->END
---
+++ Assets/StoryController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Ink.Runtime;
+
+public class StoryController : MonoBehaviour
+{
+    [SerializeField] TextAsset inkStory;
+    [SerializeField] TextMeshProUGUI storyTxt;
+    [SerializeField] Transform buttonsParent;
+
+    Story story;
+
+    void Awake() {
+        StartStory();
+    }
+
+    void StartStory() {
+        story = new Story(inkStory.text);
+        RefreshView();
+    }
+
+    void RefreshView() {
+        ResetView();
+
+        while(story.canContinue) {
+            string text = story.Continue();
+            text = text.Trim();
+            SetContentView(text);
+        }
+
+        if (story.currentChoices.Count > 0) {
+            for (int i = 0; i < story.currentChoices.Count; i++) {
+                SetChoice(i);
+            }
+        } else {
+            Debug.Log("We are done");
+        }
+    }
+
+    void ResetView() {
+        foreach (Transform button in buttonsParent) {
+            button.gameObject.SetActive(false);
+        }
+    }
+
+    void SetContentView(string text) {
+        storyTxt.text = text;
+    }
+
+    void SetChoice(int i) {
+        Choice choice = story.currentChoices[i];
+        Transform button = buttonsParent.GetChild(i);
+        button.GetComponentInChildren<TextMeshProUGUI>().text = choice.text;
+        button.GetComponentInChildren<Button>().onClick.RemoveAllListeners();
+        button.GetComponentInChildren<Button>().onClick.AddListener(delegate {
+            story.ChooseChoiceIndex(choice.index);
+            RefreshView();
+        });
+        button.gameObject.SetActive(true);
+    }
+}
